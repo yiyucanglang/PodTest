f58728b7c848e1fe4f7b901686929e3de981b29b
周义进 <547610477@qq.com>
2018-02-28 20:46:18 +0800

    *)添加files

---
+++ ZYJPrivateSpec/Classes/TestView.h
@@ -0,0 +1,12 @@
+//
+//  TestView.h
+//  Expecta
+//
+//  Created by zhangmen on 2018/1/9.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestView : UIView
+- (instancetype)initWithTitle:(NSString *)title;
+@end
---
+++ ZYJPrivateSpec/Classes/TestView.m
@@ -0,0 +1,40 @@
+//
+//  TestView.m
+//  Expecta
+//
+//  Created by zhangmen on 2018/1/9.
+//
+
+#import "TestView.h"
+
+@implementation TestView
+
+- (instancetype)init {
+    if ([super init]) {
+        [self setupUI];
+    }
+    return self;
+}
+
+- (instancetype)initWithTitle:(NSString *)title {
+    TestView * view = [self init];
+    UIButton * btn = [UIButton buttonWithType:UIButtonTypeSystem];
+    [btn setTitle:title forState:UIControlStateNormal];
+    btn.frame = CGRectMake(10, 10, 200, 50);
+    btn.backgroundColor = [UIColor grayColor];
+    [view addSubview:btn];
+    return view;
+}
+
+- (void)setupUI {
+    self.backgroundColor = [UIColor greenColor];
+}
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
