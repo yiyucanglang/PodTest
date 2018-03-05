8f3e188411cf9c0cecb28c0a1ca07cf8c32455c0
周义进 <547610477@qq.com>
2018-03-05 21:24:00 +0800

    *)添加privateframework

---
+++ PrivateFramework.framework/Headers/PrivateFramework.h
@@ -0,0 +1,19 @@
+//
+//  PrivateFramework.h
+//  PrivateFramework
+//
+//  Created by 周义进 on 05/03/2018.
+//  Copyright © 2018 DaHuanXiong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PrivateFramework.
+FOUNDATION_EXPORT double PrivateFrameworkVersionNumber;
+
+//! Project version string for PrivateFramework.
+FOUNDATION_EXPORT const unsigned char PrivateFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PrivateFramework/PublicHeader.h>
+
+
